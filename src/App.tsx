1fdea4cefcecc2419b5ed7d9c1cd6dc1e28b8c4e
Vasyl Derenko <v.derenko@5328-OC.local>
2023-01-31 11:49:13 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import image from '../public/image.png';
+import { Input } from '@/components/Input';
+
+export const App: React.FC = () => {
+  return (
+    <div className="App">
+      <span>App Component</span>
+      <img src={image} />
+      <Input />
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const MODE = argv.mode || 'production';
+
+  require('dotenv').config({
+    path: MODE === 'production' ? '.env' : '.env.dev',
+  });
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+    },
+    devtool: MODE === 'production' ? 'source-map' : 'eval-source-map',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
+    },
+    mode: MODE,
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: argv.port || 3000,
+      hot: true,
+    },
+    optimization: {
+      minimize: MODE === 'production',
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+      usedExports: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({ template: './src/index.html' }),
+      new MiniCssExtractPlugin({ filename: 'styles.css' }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'public',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+};
